66c57f961f7e0de25318c6c5b428da3d6bf43e2c
HelloTakako <24834291+HelloTakako@users.noreply.github.com>
2019-08-05 21:01:26 -0700

    Updates

---
+++ precache-manifest.c4fa37c6305cc88d6a24fc54131348bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68eb85b3a4dc91e5d15a62b8a558e3ec",
+    "url": "/seo/index.html"
+  },
+  {
+    "revision": "1a5df6ba49499bd0f7ea",
+    "url": "/seo/static/css/main.bffb7981.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/seo/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "1a5df6ba49499bd0f7ea",
+    "url": "/seo/static/js/main.fb4a0d7c.chunk.js"
+  },
+  {
+    "revision": "8fac5d745eab1877ff7f",
+    "url": "/seo/static/js/runtime~main.2df1b741.js"
+  }
+]);
