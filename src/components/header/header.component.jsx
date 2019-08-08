b0d81b7479ffeceb2e04fead0bcfaf3abee4856c
HelloTakako <24834291+HelloTakako@users.noreply.github.com>
2019-08-08 00:10:11 -0700

    changed links

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,8 +8,8 @@ const Header = () => (
             <h1>SEO Note</h1>
             <nav>
                 <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/links/">Useful Links</Link></li>
+                    <li><Link to="/seo/">Home</Link></li>
+                    <li><Link to="seo/links/">Useful Links</Link></li>
                 </ul>
             </nav>
         </header>
