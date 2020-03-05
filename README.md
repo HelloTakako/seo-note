f02e80361b438b18d716cc9190edf5f5cce1818a
HelloTakako <24834291+HelloTakako@users.noreply.github.com>
2020-03-05 15:18:38 -0800

    docs: add images, description

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-SEO Helper<br />
-
-https://hellotakako.github.io/seo/
+# SEO Note  
+**SEO Note** is an SEO reference website. If you want to check the character counts for your `<meta>` description, the app is here for you.  
+![seo note image1](/src/images/seo-note1.png)
+![seo note image2](/src/images/seo-note2.png)  
+[Demo page](https://hellotakako.github.io/seo-note/)
--- src/pages/hook/hook.component.jsx
+++ src/pages/hook/hook.component.jsx
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import config from '../../config';
 
 
+
+
 function Hook() {
   // Declare a new state variable, which we'll call "count"
-  const [count, setCount ] = useState(0);
   const configKey = config;
 
   return (
     <div>
-      <p>{configKey.MY_KEY}</p>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
+        <input name="word_input"></input>
+        <button id="submit_button">Look Up</button>
+        
+        <p>{configKey.id} {configKey.strings}</p>
     </div>
   );
 }
